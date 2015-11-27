67210a7b8b39392e0ea0fbd7d4a499ba3037727a
marcusforsberg <marcus@marcusforsberg.net>
2015-11-27 15:42:44 +0200

    * Check that ACF and Yoast are activated and refuse installation otherwise

--- yoast-acf-analysis.php
+++ yoast-acf-analysis.php
@@ -21,6 +21,39 @@ class Yoast_ACF_Analysis {
 	
 	public function admin_init() {
 		$this->plugin_data = get_plugin_data(dirname(__FILE__));
+		
+		// Require ACF and Yoast
+		if (current_user_can('activate_plugins')) {
+			$deactivate = false;
+			
+			// ACF
+			if(!is_plugin_active('advanced-custom-fields/acf.php') && !is_plugin_active('advanced-custom-fields-pro/acf.php')) {
+				add_action('admin_notices', array($this, 'require_acf'));
+				$deactivate = true;
+			}
+			
+			// Yoast
+			if(!is_plugin_active('wordpress-seo/wp-seo.php')) {
+				add_action('admin_notices', array($this, 'require_yoast'));
+				$deactivate = true;
+			}
+			
+			if($deactivate) {
+				deactivate_plugins(plugin_basename( __FILE__ )); 
+
+				if (isset($_GET['activate'])) {
+					unset($_GET['activate']);
+				}
+			}
+		}
+	}
+	
+	public function require_acf() { ?>
+		<div class="error"><p>ACF Yoast Analysis requires Advanced Custom Fields (free or pro) to be installed and activated.</p></div><?php
+	}
+	
+	public function require_yoast() { ?>
+		<div class="error"><p>ACF Yoast Analysis requires Yoast SEO 3.0+ to be installed and activated.</p></div><?php
 	}
 	
 	public function enqueue_scripts() {
